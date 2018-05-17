704d716dc63cf959b9395e7aba9bfa46bf9626bf
Anonymous Applicant <anonymous@example.com>
2018-05-16 23:26:05 -0700

    Added route to search page

---
+++ src/my_exercise/search.clj
@@ -0,0 +1,48 @@
+(ns my-exercise.search
+  (:require [hiccup.page :refer [html5]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [my-exercise.us-state :as us-state]))
+
+(defn header [_]
+  [:head
+   [:meta {:charset "UTF-8"}]
+   [:meta {:name "viewport"
+           :content "width=device-width, initial-scale=1.0, maximum-scale=1.0"}]
+   [:title "Find my next election"]
+   [:link {:rel "stylesheet" :href "default.css"}]])
+
+(defn display [_]
+  [:div {:class "display"}
+   [:h1 "Getting started"]
+   [:p "Thank you for applying to work at Democracy Works! "
+    "This coding exercise is designed to show off your ability to program web applications in Clojure. "
+    "You should spend no more than 2 hours on it and then turn it in to us "
+    "by running the command " [:code "lein submit"] " and following the instructions it prints out. "
+    "While we will be evaluating how much of the project you complete, we know that 2 hours isn't enough time to do a "
+    "thorough and complete job on all of it, and we're not expecting you to. We just want to see what you get working "
+    "in that amount of time."]
+   [:p "It is a server-side web application written in Clojure and using the "
+    [:a {:href "https://github.com/ring-clojure/ring"} "Ring"] ", "
+    [:a {:href "https://github.com/weavejester/compojure"} "Compojure"] ", and "
+    [:a {:href "https://github.com/weavejester/hiccup"} "Hiccup"] " libraries."
+    "You should feel free to use other libraries as you see fit."]
+   [:p "Right now the form below submits to a missing route in the app. To complete the exercise, do the following:"]
+   [:ul
+    [:li "Create the missing /search route"]
+    [:li "Ingest the incoming form parameters"]
+    [:li "Derive a basic set of OCD-IDs from the address (see below for further explanation)"]
+    [:li "Retrieve upcoming elections from the Democracy Works election API using those OCD-IDs"]
+    [:li "Display any matching elections to the user"]]
+   [:p "You will get bonus points for:"
+    [:ul
+     [:li "Documenting your code"]
+     [:li "Adding tests for your code"]
+     [:li "Standardizing and/or augmenting the address data to derive more OCD division IDs (e.g. county and "
+      "legislative districts)"]
+     [:li "Noting additional features or other improvements you would make if you had more time"]]]])
+
+
+(defn page [request]
+  (html5
+   (header request)
+   (display request)))
